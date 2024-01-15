75e59dec6dba7fc386e1f9d4def04f6f8e04525f
suricaoo <156049010+suricaoo@users.noreply.github.com>
2024-01-15 09:11:45 +0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,39 @@
+function setup() {
+  
+      createCanvas(400, 400);
+      background(0);
+      noLoop();
+    }
 
+    function draw() {
+      generateArt();
+      
+    }
+
+    function generateArt() {
+      for (let i = 0; i < 500; i++) {
+        let x = random(width);
+        let y = random(height);
+        let size = random(50,50);
+        let col = color(random(255), random(255), random(255), 100);
+
+        drawShape(x, y, size, col);
+      }
+    }
+
+    function drawShape(x, y, size, col) {
+      
+    
+      fill(col, random(0,6));
+      noStroke();
+      let choice = floor(random(200));
+      rect(x, y, size, size);
+       
+    }     
+        
+       
+        
+      
+      
+
+    
