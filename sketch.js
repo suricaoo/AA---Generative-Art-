c6d6c968362eeb6de81e2b81af374d7f0a2f6645
suricaoo <156049010+suricaoo@users.noreply.github.com>
2024-01-17 11:28:11 +0700

    Add triangles and arc to the drawing for more diversity and texture

--- sketch.js
+++ sketch.js
@@ -1,37 +1,47 @@
 function setup() {
-  
-      createCanvas(400, 400);
-      background(0);
-      noLoop();
-    }
+  createCanvas(400, 400);
+  background(0);
+  noLoop();
+}
 
-    function draw() {
-      generateArt();
-      
-    }
+function draw() {
+  generateArt();
+}
 
-    function generateArt() {
-      for (let i = 0; i < 500; i++) {
-        let x = random(width);
-        let y = random(height);
-        let size = random(50,50);
-        let col = color(random(255), random(255), random(255), 100);
+function generateArt() {
+  for (let i = 0; i < 500; i++) {
+    let x = random(width);
+    let y = random(height);
+    let size = random(50, 50);
+    let col = color(random(255), random(255), random(255), 100);
 
-        drawShape(x, y, size, col);
-      }
-    }
+    drawRandomShape(x, y, size, col);
+  }
+}
 
-    function drawShape(x, y, size, col) {
-      
+function drawRandomShape(x, y, size, col) {
+  fill(col, random(0, 6));
+  noStroke();
+  let choice = floor(random(3));
+
+  if (choice === 0) {
+    rect(x, y, size, size);
+  } 
+  else if (choice === 1) 
+  {
     
-      fill(col, random(0,6));
-      noStroke();
-      let choice = floor(random(200));
-      rect(x, y, size, size);
-       
-    }     
-        
-       
+    let height = (sqrt(3) / 2) * size;
+    triangle(x, y, x + size, y, x + size / 2, y - height);
+  } 
+  else
+  {
+    
+    let startAngle = random(TWO_PI);
+    
+    let endAngle = startAngle + random(HALF_PI, TWO_PI);
+    arc(x, y, size, size, startAngle, endAngle);
+  }
+}
         
       
       
